d1afa5ee40bf4076c4745558d153093056e5c120
Himanshu B A <1ds23is407@dsce.edu.in>
2026-01-06 00:21:53 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eduvoice/', // Repository name for GitHub Pages
+  base: '/EduVoice/', // Repository name for GitHub Pages
   server: {
     port: 3000,
     open: true
